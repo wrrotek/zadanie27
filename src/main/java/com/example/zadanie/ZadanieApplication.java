57f327e7251b5b6ab166cbccb7188585e038a4f3
Kornelia Chilczuk Kudan <kornelia.chilczuk@gmail.com>
2021-08-30 22:04:47 +0200

    Initial commit

---
+++ src/main/java/com/example/zadanie/ZadanieApplication.java
@@ -0,0 +1,13 @@
+package com.example.zadanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadanie/ZadanieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
